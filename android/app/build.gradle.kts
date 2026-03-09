d2d9d4be493041b53c810a377ecf9bae0922d5a6
Mohammad Falah <152405726+Mohhp@users.noreply.github.com>
2026-03-09 12:16:43 +0000

    chore: bump versionCode for Play Store release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "io.github.mohhp.essentialduas"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.1.0"
+        versionCode = 11
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
